8ec0b3b453f65cdd673a27a1476b40db7bcb386b
IoaMertz <119110449+IoaMertz@users.noreply.github.com>
2023-07-01 17:57:12 +0300

    controller now await ?? the completion of consumer?? . and if not completed in 0.2 seconds return message

--- Api/Controllers/CalculateController.cs
+++ Api/Controllers/CalculateController.cs
@@ -21,11 +21,21 @@ namespace CalculationsApi.Controllers
         public async Task<ActionResult<CalculationResponseObject>> Calculate(
             [FromBody] CalculationRequestObject calculateRequestObject)
         {
-            await _mediator.Send(new CalculationRequestCommand(calculateRequestObject.Number,calculateRequestObject.Service));
-            // there we sould wait for the result and send the response
+            var sendTask =  _mediator.Send(new CalculationRequestCommand(
+                calculateRequestObject.Number,calculateRequestObject.Service));
 
+            var completedTask = await Task.WhenAny(sendTask, Task.Delay(TimeSpan.FromMinutes(0.1)));
 
-            return Ok();
+            if (completedTask == sendTask)
+            {
+                // The sendTask completed within the timeout duration
+                var result = await sendTask;
+
+                // Process the result and send the appropriate response
+                return Ok("kati");
+            }
+
+            return Ok("skata");
         }
     }
 }
